65afce0279282110bca502de8760231b6789da15
Dmitriy Tutichenko <tutichenko@gmail.com>
2024-05-27 18:26:51 +0300

    wip: home tab markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-require('./styles.css');
+require('./styles/styles.css');
+require('./styles/header.css');
+require('./styles/footer.css');
+require('./styles/home.css');
+
 
 console.log("Hello World!");
