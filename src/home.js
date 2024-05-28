0402e321adc2d090ef1c8ed8fd53464db4766a43
Dmitriy Tutichenko <tutichenko@gmail.com>
2024-05-28 15:00:06 +0300

    wip: render home page

---
+++ src/home.js
@@ -0,0 +1,14 @@
+/*  <div class="home">
+        <h1>Restaurant Page</h1>
+        <p>Pasta la vista baby, pasta la vista...</p>
+    </div> */
+
+
+const homePage = document.createElement('div');
+homePage.classList.add('home');
+homePage.innerHTML = `<h1>Restaurant Page</h1>
+<p>Pasta la vista baby, past la vista...</p>
+`;
+
+export { homePage };
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { pastas } from './pastas.js';
+import { homePage } from './home.js';
 
 require('./styles/styles.css');
 require('./styles/header.css');
@@ -6,5 +7,16 @@ require('./styles/footer.css');
 require('./styles/home.css');
 require('./styles/menu.css');
 
-// pastas.forEach(console.log);
-console.log(pastas);
+
+const homeBtn = document.querySelector('#home-btn');
+const menuBtn = document.querySelector('#menu-btn');
+const aboutBtn = document.querySelector('#about-btn');
+const content = document.querySelector('#content');
+
+content.appendChild(homePage);
+
+homeBtn.addEventListener('click', () => {
+    console.log('home');
+    content.innerText = '';
+    content.appendChild(homePage);
+});
